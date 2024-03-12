e8e7dc758012e703a5da33d4c96147e71e1b2eaa
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-03-11 23:45:30 -0300

    entrega finalizada

--- desafio05/src/Dao/ManagerMongo/ProductManagerMongo.js
+++ desafio05/src/Dao/ManagerMongo/ProductManagerMongo.js
@@ -2,35 +2,9 @@ import { productsModel } from "../../db/models/products.model.js";
 
 export default class ProductManager {
 
-
-    async getProducts(limit, page, sort, query) {
+    async getProducts() {
         try {
-            const search = query ? {
-                stock: { $gt: 0 },
-                $or: [
-                    //devuelve todos los productos que tengan el query en el titulo o en la categoria
-                    { category: { $regex: query, $options: 'i' } },
-                    { title: { $regex: query, $options: 'i' } },
-                ]
-            } : {
-                //devuelve todos los productos que tengan stock mayor a 0
-                stock: { $gt: 0 }
-            }
-
-            if (sort === 'asc') {
-                sort = { price: 1 };
-            } else if (sort === 'desc') {
-                sort = { price: -1 };
-            }
-
-            const options = {
-                page: page || 1,
-                limit: limit || 10,
-                sort: sort,
-                lean: true,
-            }
-
-            const allProducts = await productsModel.paginate(search, options)
+            const allProducts = await productsModel.find();
             return allProducts;
         } catch (error) {
             console.log(error);
@@ -57,19 +31,19 @@ export default class ProductManager {
     }
 
     async updateProduct(id, update) {
-        try {
-            const updatedProduct = await productsModel.findOneAndUpdate(id, update, { new: true });
+        try{
+            const updatedProduct = await productsModel.findOneAndUpdate(id, update, {new: true});
             return updatedProduct;
-        } catch (error) {
+        }catch(error){
             console.log(error);
         }
     }
 
     async deleteProduct(id) {
-        try {
+        try{
             const product = await productsModel.findByIdAndDelete(id);
             return product;
-        } catch (error) {
+        }catch(error){
             console.log(error);
         }
     }
--- desafio05/src/routes/products.router.js
+++ desafio05/src/routes/products.router.js
@@ -1,38 +1,28 @@
-import { Router } from 'express';
+import { Router } from "express";
 import ProductManager from "../Dao/ManagerMongo/ProductManagerMongo.js";
 
-
+// importamos el manejador de productos para poder usarlo en los endpoints de la API de productos (REST)
 const router = Router();
+
+// Creamos una instancia del manejador de productos para poder usarla en los endpoints de la API de productos (REST)
 const productManager = new ProductManager('./productos.json');
 
-//Endpoint para obtener productos (o los n productos si se pasa parámetro limit)
+// Endpoint para obtener todos los productos (o los primeros N productos si se pasa el parámetro limit en la query string)
 router.get('/', async (req, res) => {
     try {
-        const { limit, page, sort, query } = req.query;
+        const limit = req.query.limit;
 
-        const products = await productManager.getProducts(limit, page, sort, query);
+        const products = await productManager.getProducts();
 
-        products.docs = products.docs.map(product => {
-            const { _id, title, description, price, code, stock, category, thumbnail } = product;
-            return { _id, title, description, price, code, stock, category, thumbnail };
-        });
-
-        const info = {
-            totalPages: products.totalPages,
-            prevPage: products.prevPage,
-            nextPage: products.nextPage,
-            page: products.page,
-            hasPrevPage: products.hasPrevPage,
-            hasNextPage: products.hasNextPage,
-            prevLink: products.hasPrevPage ? `http://localhost:9090/api/products?page=${products.prevPage}` : null,
-            nextLink: products.hasNextPage ? `http://localhost:9090/api/products?page=${products.nextPage}` : null
+        if (limit) {
+            const limitedProducts = products.slice(0, limit);
+            res.status(200).send({ status: "success", payload: limitedProducts });
+        } else {
+            res.status(200).send({ status: "success", payload: products });
         }
-
-        res.status(200).send({ status: "success", payload: products.docs, info });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ status: "error", error: "Error al obtener los productos" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ status: "error", error: 'Error al obtener los productos' });
     }
 });
 
@@ -40,70 +30,77 @@ router.get('/', async (req, res) => {
 router.get('/:pid', async (req, res) => {
     try {
         const { pid } = req.params;
+
+        // const product = await productManager.getProductById(parseInt(pid));
         const product = await productManager.getProductById(pid);
 
         if (product) {
             res.status(200).send({ status: "success", payload: product });
         } else {
-            res.status(404).send({ status: "error", error: "Producto no encontrado" });
+            res.status(404).send({ status: "error", error: 'Producto no encontrado' });
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ status: "error", error: "Error al obtener el producto" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ status: "error", error: 'Error al obtener el producto' });
     }
-})
+});
 
-//Endpoint para agregar un producto
+// Endpoint para agregar un producto (el producto se pasa en el body de la request)
 router.post('/', async (req, res) => {
     try {
         const product = req.body;
         if (!product.title || !product.description || !product.price || !product.code || !product.stock || !product.category) {
-            return res.status(400).send({ status: "error", error: "Faltan datos del producto" });
+            res.status(400).send({ status: "error", error: 'Todos los campos son obligatorios' });
+            return;
         }
         const newProduct = await productManager.addProduct(product);
         if (!newProduct) {
-            return res.status(400).send({ status: "error", error: "El producto ya existe" });
+            res.status(400).send({ status: "error", error: 'El código del producto ya existe' });
         } else {
             res.status(201).send({ status: "success", payload: newProduct });
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ status: "error", error: "Error al agregar el producto" });
+    } catch (err) {
+        console.error(err);
+        res.status(500)
+            .send({ status: "error", error: 'Error al agregar el producto' });
     }
 });
 
-//Endpoint para actualizar un producto
+// Endpoint para actualizar un producto (el producto se pasa en el body de la request) (el id del producto se pasa como parámetro en la URL)
 router.put('/:pid', async (req, res) => {
     try {
         const { pid } = req.params;
         const product = req.body;
-        const updatedProduct = await productManager.updateProduct({ _id, pid });
-
+        const updatedProduct = await productManager.updateProduct(
+            { _id: pid },
+            product
+        );
         if (!updatedProduct) {
-            res.status(404).send({ status: "error", error: "Producto no encontrado" });
+            res.status(404).send({ status: "error", error: 'Producto no encontrado' });
         } else {
             res.status(201).send({ status: "success", payload: updatedProduct });
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ status: "error", error: "Error al actualizar el producto" });
+    } catch (err) {
+        console.error(err);
+        res.status(500)
+            .send({ status: "error", error: 'Error al actualizar el producto' });
     }
 });
 
-//Endpoint para borrar un producto
+// Endpoint para borrar un producto (el id del producto se pasa como parámetro en la URL)
 router.delete('/:pid', async (req, res) => {
     try {
         const { pid } = req.params;
         const deletedProduct = await productManager.deleteProduct(pid);
-
         if (!deletedProduct) {
-            res.status(404).send({ status: "error", error: "Producto no encontrado" });
+            res.status(404).send({ status: "error", error: 'Producto no encontrado' });
         } else {
-            res.status(200).send({ status: "success", payload: deletedProduct });
+            res.status(201).send({ status: "success", payload: deletedProduct });
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ status: "error", error: "Error al borrar el producto" });
+    } catch (err) {
+        console.error(err);
+        res.status(500)
+            .send({ status: "error", error: 'Error al borrar el producto' });
     }
 });
 
