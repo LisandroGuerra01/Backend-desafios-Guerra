eabadb32ab5f78a9732dc5553fa6a4221810c55a
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-03-25 20:31:36 -0300

    add views router

--- desafio05/src/routes/views.router.js
+++ desafio05/src/routes/views.router.js
@@ -1,1 +1,86 @@
-import
+import { Router } from "express";
+import ProductManager from "../Dao/ManagerMongo/ProductManagerMongo";
+import CartsManager from "../Dao/ManagerMongo/CartManagerMongo";
+import UsersManager from "../Dao/ManagerMongo/UsersManagerMongo";
+import { auth, isLogged } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+const usersManager = new UsersManager();
+
+router.get("/chat", async (req, res) => {
+    res.render("chat", {
+        title: "Chat",
+        StyleSheet: "chat.css",
+    });
+});
+
+//Endpoint para visualizar todos los productos
+router.get("/products", auth, async (req, res) => {
+    const { userId, isAdmin, role } = req.session;
+    const userLogged = await usersManager.getUserById(userId);
+    const { first_name, last_name, email, age } = userLogged;
+
+    const productManager = new ProductManager();
+    const products = await productManager.getProducts(2);
+
+    res.render("products", { products, first_name, last_name, email, age, isAdmin, role });
+});
+
+//Endpoint para visualizar todos los productos con paginación
+router.get("/products/page/:page", auth, async (req, res) => {
+    const productManager = new ProductManager();
+    const products = await productManager.getProducts(2, page);
+
+    res.render("products", { products });
+});
+
+//Endpoint para visualizar un producto en particular
+router.get("/products/:pid", auth, async (req, res) => {
+    const productManager = new ProductManager();
+    const product = await productManager.getProductById(req.params.pid);
+
+    const { _id, title, description, price, code, stock, category, thumbnail } = product;
+
+    res.render("product", { id: __dirname, title, description, price, code, stock, category, thumbnail });
+});
+
+//Endpoint para visualizar el carrito de compras
+router.get("/carts/:cid", auth, async (req, res) => {
+    const cartManager = new CartsManager();
+    const cart = await cartManager.getCartById(req.params.cid);
+
+    const { products } = cart;
+
+    res.render("cart", { cart });
+});
+
+//Endpoint para registro de usuario
+router.get("/register", isLogged, (req, res) => {
+    res.render("register");
+});
+
+//Endpoint para login de usuario
+router.get("/login", isLogged, (req, res) => {
+    res.render("login");
+});
+
+//Endpoint para perfil de usuario
+router.get("/profile", auth, async (req, res) => {
+    const { userId, isAdmin, role } = req.session;
+    const userLoggerd = await usersManager.getUserById(userId);
+    const { first_name, last_name, email, age } = userLoggerd;
+
+    res.render("profile", { first_name, last_name, email, age, isAdmin, role });
+});
+
+//Endpoint para error de registro
+router.get("/errorRegister", (req, res) => {
+    res.render("errorRegister");
+});
+
+//Endpoint para error de login
+router.get("/errorLogin", (req, res) => {
+    res.render("errorLogin");
+});
+
+export default router;
