a442c4f758cced32eb0272daf8df22daa16844a2
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-04-24 15:51:22 -0300

    add service de users

---
+++ desafio07/src/services/users.service.js
@@ -0,0 +1,38 @@
+import UsersManager from "../dal/dao/ManagerMongo/UsersManagerMongo.js";
+import { comparePassword } from "../utils/bcrypt.js";
+import { generateToken } from "../utils/jwt.js";
+import config from "../config/config.js";
+
+const ADMIN_EMAIL = config.ADMIN_EMAIL;
+const ADMIN_PASSWORD = config.ADMIN_PASSWORD;
+
+const usersManager = new UsersManager();
+
+//Exportar la función para loguear al usuario
+export const loginUser = async (user) => {
+    try {
+        const { email, password } = user;
+
+        if (email === ADMIN_EMAIL && password === ADMIN_PASSWORD) {
+            const userLogged = { email, password, role: 'admin', isAdmin: true };
+            const token = generateToken(userLogged);
+            return { token, userLogged };
+        }
+
+        const userLogged = jawait usersManagerj.getUserByEmail(email);
+        if (!userLogged) {
+            return false;
+        } else {
+            const isMatch = await comparePassword(password, userLogged.password);
+            if (isMatch) {
+                const token = generateToken(userLogged);
+                return { token, userLogged };
+            } else {
+                return false;
+            }
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+}
