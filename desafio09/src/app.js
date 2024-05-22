d373f0019515c2d0ab43485241650121e158a293
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-05-21 21:43:15 -0300

    add config de logger

---
+++ desafio09/src/app.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import config from './config/config.js';
+import connectDB from './dal/mongoDB/dbConfig.js';
+import cookieParser from 'cookie-parser';
+import apiRouter from './routes/api.router.js';
+import compression from 'express-compression';
+import { errorMiddleware } from './middlewares/error.middleware.js';
+import { logger } from './utils/logger.utils.js';
+
+
+const app = express();
+const NODE_ENV = config.node_env
+const log = logger(NODE_ENV);
+
+app.use(compression());
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api', apiRouter);
+
+app.use(errorMiddleware);
+
+const PORT = config.port;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ desafio09/src/utils/logger.utils.js
@@ -0,0 +1,62 @@
+import winston from 'winston';
+
+//Defino los niveles de prioridad de los msj de log de menor a mayor
+const levels = {
+    debug: 0,
+    http: 1,
+    info: 2,
+    warn: 3,
+    error: 4,
+    fatal: 5
+};
+
+//Defino los colores de los msj de log para cada nivel deprioridad de menor a mayor
+const colors = {
+    debug: 'blue',
+    http: 'green',
+    info: 'cyan',
+    warn: 'yellow',
+    error: 'red',
+    fatal: 'magenta'
+};
+
+//Crear el logger de desarrollo
+const developmentLogger = winston.createLogger({
+    levels,
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        })
+    ]
+});
+
+//Crear el logger de produccion
+const productionLogger = winston.createLogger({
+    levels,
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'logs/combined.log' })
+    ]
+});
+
+//Exporto el logger correspondiente según el entorno de ejecución (development o production)
+export const logger = (env) => {
+    if (env === 'development') {
+        return developmentLogger;
+    } else {
+        return productionLogger;
+    }
+}
