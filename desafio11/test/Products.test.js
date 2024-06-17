ecf4a96ff694517f75752fb4bcbc44e7eb7fa76a
lisandroguerra01 <lisandroguerra01@gmail.com>
2024-06-17 16:03:24 -0300

    test ejecutados

---
+++ desafio11/test/Products.test.js
@@ -0,0 +1,19 @@
+import productsMongo from '../src/DAL/DAOs/productsDaos/productsMongo.js';
+import Assert from 'assert';
+import connectDB from '../src/DAL/mongoDB/dbConfig.js';
+
+connectDB();
+
+const assert = Assert.strict;
+
+describe('Testing Products DAO', () => {
+    before(function () {
+        console.log("VER: ", productsMongo);
+    })
+    it('El Dao debe poder obtener los productos en formato de array', async () => {
+        const result = await productsMongo.findAll();
+        assert.strictEqual(Array.isArray(result), true);
+    })
+    beforeEach(function () {
+    })
+}); 
